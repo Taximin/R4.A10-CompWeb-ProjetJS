413dab5f81371820550f3dd6eb23ee763da562eb
Naiizey <nairolfdu22@gmail.com>
2023-03-06 09:48:43 +0100

    fix getLanguages & getCurrencies

--- Country.js
+++ Country.js
@@ -29,78 +29,78 @@ export default class Country
             this.topLevelDomain = topLevelDomain;
         }
         
-        getPopDensity()
+    getPopDensity()
+    {
+        return (this.population / this.area).toFixed(2);
+    }
+    
+    getBorders()
+    {
+        if (this.borders != undefined)
         {
-            return (this.population / this.area).toFixed(2);
+            return this.borders;
         }
-        
-        getBorders()
-        {
-            if (this.borders != undefined)
-            {
-                return this.borders;
+        return [];
+    }
+    
+    toString()
+    {
+        return  "Code Alpha 3: " + this.codeAlpha3 + 
+        "\nNom:" + this.name + 
+        "\nContinent: " + this.continent + 
+        "\nPopulation: " + this.population + 
+        "\nDensité de population: " + this.getPopDensity() + 
+        "\nCapitale: " + this.capital + 
+        "\nGentilé: " + this.demonym + 
+        "\nTop Level Domain: " + this.topLevelDomain + 
+        "\nPays limitrophes: " + this.getBorders();
+    }
+    
+    static fill_db()
+    {
+        for(let country of countries){
+            Country.allCountries[country.alpha3Code] = new Country(
+                country.alpha3Code,
+                country.area,
+                country.borders,
+                country.capital,
+                country.continent,
+                country.demonym,
+                country.name,
+                country.population,
+                country.topLevelDomain
+                );
+            if(country.currencies != undefined && country.currencies.length < 2){
+                let currency = country.currencies[0];
+                if(Country.allCurrencies[currency.code] == undefined && currency.code != ""){
+                    Country.allCurrencies[currency.code] = new Currency(currency.code, currency.symbol);
+                }
             }
-            return [];
-        }
-        
-        toString()
-        {
-            return  "Code Alpha 3: " + this.codeAlpha3 + 
-            "\nNom:" + this.name + 
-            "\nContinent: " + this.continent + 
-            "\nPopulation: " + this.population + 
-            "\nDensité de population: " + this.getPopDensity() + 
-            "\nCapitale: " + this.capital + 
-            "\nGentilé: " + this.demonym + 
-            "\nTop Level Domain: " + this.topLevelDomain + 
-            "\nPays limitrophes: " + this.getBorders();
-        }
-        
-        static fill_db()
-        {
-            for(let country of countries){
-                Country.allCountries[country.alpha3Code] = new Country(
-                    country.alpha3Code,
-                    country.area,
-                    country.borders,
-                    country.capital,
-                    country.continent,
-                    country.demonym,
-                    country.name,
-                    country.population,
-                    country.topLevelDomain
-                    );
-
-                if(country.currencies != undefined && country.currencies.length < 2){
-                    let currency = country.currencies[0];
+            
+            else if(country.currencies != undefined && country.currencies.length > 1){
+                for(let currency of country.currencies){
                     if(Country.allCurrencies[currency.code] == undefined && currency.code != ""){
                         Country.allCurrencies[currency.code] = new Currency(currency.code, currency.symbol);
                     }
                 }
-                
-                else if(country.currencies != undefined && country.currencies.length > 1){
-                    for(let currency of country.currencies){
-                        if(Country.allCurrencies[currency.code] == undefined && currency.code != ""){
-                            Country.allCurrencies[currency.code] = new Currency(currency.code, currency.symbol);
-                        }
-                    }
-                }      
-                
-                let languages = country.languages;
-                for(let language of languages){
-                    if(Country.allLanguages[language.iso639_2] == undefined && language.iso639_2 != ""){
-                        Country.allLanguages[language.iso639_2] = new Language(language.iso639_2, language.name);
-                    }
+            }      
+            
+            let languages = country.languages;
+            for(let language of languages){
+                if(Country.allLanguages[language.iso639_2] == undefined && language.iso639_2 != ""){
+                    Country.allLanguages[language.iso639_2] = new Language(language.iso639_2, language.name);
                 }
             }
         }
-    
+    }
+
     getLanguages()
     {
-        
+        return Country.allLanguages[this];
     }
     
     getCurrencies()
     {
+        return Country.allCurrencies[this];
     }
 }
